314f993d5b07996b522c149c64a2350f23b896c5
Mark Lee <markleechuyong@gmail.com>
2016-10-16 22:18:09 +0100

    initial work to switch to webpack

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
+import { HeroesComponent } from "./components/heroes/heroes.component";
+import { HeroService } from "./services/hero.service";
+import { AppRoutingModule } from "./app-routing.module";
+import { NavigationComponent } from "./components/navigation/navigation.component";
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { LoginComponent } from "./components/login/login.component";
+import { AboutPageComponent } from "./components/about-page/about-page.component";
+import { ProjectsPageComponent } from "./components/projects-page/projects-page.component";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    ProjectsPageComponent,
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    LoginComponent,
+    NavigationComponent
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
