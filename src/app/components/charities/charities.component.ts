28bce0a2badb4c5e8cb8bbfe24f65278487bfb77
Mark Lee <markleechuyong@gmail.com>
2016-10-27 16:00:58 +0100

    [mark, royson] Improvements to charity display page

--- src/app/components/charities/charities.component.ts
+++ src/app/components/charities/charities.component.ts
@@ -11,15 +11,28 @@ import { CharityService } from '../../services/charity.service';
 
 
 export class CharitiesComponent implements OnInit {
+  private stepSize:number = 5;
+  
   charities:Charity[] = null;
+  displayCharities:Charity[] = null;
+  leftPos:number = 0;
+  rightPos:number = this.stepSize;
   
   constructor(private router:Router,
               private charityService:CharityService) {
   }
   
   ngOnInit():void {
+    // this.charityService.getCharities()
+    //   .then(charities => this.charities = charities);
     this.charityService.getCharities()
-      .then(charities => this.charities = charities);
+      .then(charities => {
+        this.charities = charities;
+        this.displayCharities = this.charities.slice(this.leftPos, this.rightPos);
+        if (this.charities.length < this.rightPos) {
+          this.rightPos = this.charities.length - 1;
+        }
+      });
   }
   
   goToDetail(charity:Charity):void {
@@ -29,6 +42,25 @@ export class CharitiesComponent implements OnInit {
   
   updateCharities():void {
     this.charityService.getCharities()
-      .then(charities => this.charities = charities);
+      .then(charities => {
+        this.charities = charities;
+        // Update the displayed charities based on existing filter
+      });
+  }
+  
+  moveRight():void {
+    if (this.leftPos + this.stepSize < this.charities.length - 1) {
+      this.leftPos += this.stepSize;
+      this.rightPos += this.stepSize;
+      this.displayCharities = this.charities.slice(this.leftPos, this.rightPos);
+    }
+  }
+  
+  moveLeft():void {
+    if (this.rightPos - this.stepSize > 0) {
+      this.leftPos -= this.stepSize;
+      this.rightPos -= this.stepSize;
+      this.displayCharities = this.charities.slice(this.leftPos, this.rightPos);
+    }
   }
 }
