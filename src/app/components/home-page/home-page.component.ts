06f235f811c59fc3a51d3a30dfc5fd46938dfdcb
Lee De Sheng Royson <de.lee14@imperial.ac.uk>
2016-10-27 22:53:28 +0100

    [royson][mark] Setup basic FB authentication.

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+=======
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Hero } from "../../constants/data-types";
+>>>>>>> [royson][mark] Setup basic FB authentication.
 
 @Component({
   selector: 'home-page',
@@ -8,6 +14,7 @@ import { Router } from '@angular/router';
 })
 
 export class HomePageComponent {
+
   constructor(private router:Router) {
   }
 }
