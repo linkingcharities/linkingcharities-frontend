acb9d705dea768c9f008ea6bac3807f08eef6884
Mingfei Yao <ming@mingfeis-mbp.home>
2016-11-29 16:48:25 +0000

    personalised thankyou

--- src/app/components/thank-you/thank-you.component.ts
+++ src/app/components/thank-you/thank-you.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ToasterService } from 'angular2-toaster/angular2-toaster';
+import { Response, Http, RequestOptions } from '@angular/http';
+import { API_URL } from '../../constants/config';
 
 @Component({
   selector: 'thank-you',
@@ -15,15 +18,33 @@ export class ThankYouComponent implements OnInit {
   private sub: any;
 
   constructor(private router:Router,
-              private ar:ActivatedRoute){}
+              private ar:ActivatedRoute,
+              private toasterService:ToasterService,
+              private http:Http){}
 
   ngOnInit():void {
     this.sub = this.ar.params.subscribe(params => {
         this.payment = +params['id'];
     });
+    
+    let user:String = localStorage.getItem("user");
+    if (user) {
+      this.http.post(API_URL + '/show_payment', 
+      {'username': user, 'payment': this.payment.toString()})
+      .toPromise()
+      .then((res:Response) => {
+        console.log(res.json());
+        let data = res.json();
+        this.business = data['charity'];
+        this.amount = data['amount'];
+      }).catch((err:Error) => { 
+      this.toasterService.pop('success', '', 'Thank you for your donation.');
+    });
 //    this.business = window.location.search().business;
 //    this.amount = window.location.search().amount;
-    console.log(this.payment);
+    } else {
+      this.router.navigate(['homepage']);
+    }
   }
 
   goHome():void {
