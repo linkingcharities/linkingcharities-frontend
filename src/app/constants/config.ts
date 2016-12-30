505f7256ce8c46adc3e201c260ba47641ec86727
Mingfei Yao <my1914@ic.ac.uk>
2016-12-30 20:09:24 +0000

    request api not https

--- src/app/constants/config.ts
+++ src/app/constants/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.ENV == 'production' ? 'http://138.68.147.114:8000/api' : 'http://localhost:8000/api';
+export const API_URL = process.env.ENV == 'production' ? 'https://138.68.147.114:8000/api' : 'https://localhost:8000/api';
 export const IMAGE_URL = 'https://dl.dropboxusercontent.com/u/49220569/charilink';
